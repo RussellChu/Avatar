b51be28e88b8988938a173313a2354cf06f905b6
RussellChu <cklpimatch@gmail.com>
2017-08-09 17:01:32 +0800

    update web

---
+++ Web/Test2/main.js
@@ -0,0 +1,33 @@
+console.log( 'ABC' );
+var objCode = '<div>\
+</div>';
+
+var ClassCreator = function () {
+	;
+};
+
+var clsA = Class.create(
+);
+
+var clsA = {};
+clsA.newObj = function ( p_valueA ) {
+	
+};
+
+
+var a = clsA.newObj();
+
+var ITest = function () {
+	;
+};
+
+ITest.prototype = {
+	methodA: function ():int
+};
+
+
+var Main = function () {
+	
+};
+
+class 
