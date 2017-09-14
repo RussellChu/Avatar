efc396d019b3a01f7d7901206a0a002106926cb1
RussellChu <cklpimatch@gmail.com>
2017-09-14 17:50:04 +0800

    update avatar web

--- Web/Test/nodejs/app.js
+++ Web/Test/nodejs/app.js
@@ -1,232 +1,411 @@
-var express = require('express');
-var app = express();
+const contentTypes = require('./utils/content-types');
+const env = process.env;
+const express = require('express');
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
+const sysInfo = require('./utils/sys-info');
 
-var http = require('http');
-var fs = require('fs');
-
-// bitsadmin /transfer wcb /priority high https://cn.unlight.jp/public/image/cc17_01.swf.8cf455dde4b79c49ae03de16ea22f22a D:\Download\cc17_01.swf
-
-var TASK = {
-	  NONE: 0
-	, FETCH_SWF: 1
-	, TEST: 2
-};
-
-var currTask = TASK.TEST;
-
-var task_fetch_swf = function ( callback ) {
-	var swfList = [];
-	var swfMap = {};
-	var harList = [];
-	var saveFolder = 'D:\\Download\\unlight\\';
+var validator = (function () {
+	var inst = {};
 	
-	var fetchSwf = function () {
-		if ( harList.length == 0 ) {
-			var str = "";
-			for(var i = 0; i < swfList.length; i++) {
-				str += 'bitsadmin /transfer wcb /priority high ' + swfList[i].url + ' ' + saveFolder + swfList[i].name + '<br/>';
+	var check = function ( obj, arr ) {
+		if ( !obj || !( typeof obj === 'object' ) || !arr || !( arr instanceof Array ) ) {
+			return false;
+		}
+		for(var i = 0; i < arr.length; i++) {
+			var item = arr[i];
+			if ( !item.hasOwnProperty( 'name' ) || typeof item.name !== 'string' ) {
+				return false;
 			}
-			console.log( 'swf found >> ' + swfList.length );
-			callback( str );
+			
+			if ( !obj.hasOwnProperty( item.name ) ) {
+				return false;
+			}
+			
+			if ( !item.hasOwnProperty( 'type' ) || typeof item.type !== 'string' ) {
+				continue;
+			}
+			
+			if ( typeof obj[item.name] !== item.type ) {
+				return false;
+			}
+		}
+		return true;
+	};
+	
+	inst.check = check;
+	
+	return inst;
+} ());
+
+var eventDispatcher = (function () {
+	var inst = {};
+	var map = {};
+	var dispose = function () {
+		inst = null;
+		map = null;
+	};
+	var add = function ( action, listener ) {
+		if ( !map || typeof action !== 'string' || action === '' || typeof listener !== 'function' ) {
+			return;
+		}
+		if ( !map[action] ) {
+			map[action] = [];
+		}
+		
+		map[action].push( listener );
+	};
+	var remove = function ( action, listener ) {
+		if ( !map ) {
+			return;
+		}
+		if ( typeof action !== 'string' || action === '' ) {
+			map = {};
+			return;
+		}
+		if ( !map[action] ) {
+			return;
+		}
+		if ( typeof listener !== 'function' ) {
+			map[action] = [];
+			return;
+		}
+		var i = 0;
+		var list = map[action];
+		for(i = 0; i < list.length; i++) {
+			if ( list[i] === listener ) {
+				list.splice( i, 1 );
+				return;
+			}
+		}
+	};
+	var dispatch = function ( action, data ) {
+		if ( !map || typeof action !== 'string' || action === '' || !map[action] ) {
+			return;
+		}
+		var i = 0;
+		var list = map[action];
+		for(i = 0; i < list.length; i++) {
+			var listener = list[i];
+			listener( data );
+		}
+	};
+	inst.dispose = dispose;
+	inst.add = add;
+	inst.dispatch = dispatch;
+	inst.remove = remove;
+	return inst;
+} ());
+
+var fileCacher = ( function () {
+	var inst = {};
+	
+	var cache_max = 10;
+	var cache_list = [];
+	var cache_map = {};
+	
+	var init = function ( p_cache_max ) {
+		cache_max = p_cache_max;
+	};
+	
+	var clear = function () {
+		for(var key in cache_map) {
+			delete cache_map[key];
+		}
+		
+		cache_list = [];
+		cache_map = {};
+	};
+	
+	var getFile = function ( path, forceReload, callback ) {
+		if ( typeof callback !== 'function' ) {
 			return;
 		}
 		
-		var harName = harList.pop();
-		fs.readFile('./' + harName, 'utf8', function (err, rslt) {
+		if ( typeof path !== 'string' || !path ) {
+			callback( { msg: 'Invalid Path' }, null );
+			return;
+		}
+		
+		if ( cache_map.hasOwnProperty( path ) && !forceReload ) {
+			console.log( '[fileCacher] Use >> ' + path );
+			callback( null, cache_map[path] );
+			return;
+		}
+		
+		fs.readFile( path, function(err, data) {
 			if (err) {
-				;
+				callback( err, data );
+				return;
+			}
+			
+			if ( cache_list.length >= cache_max ) {
+				var remove_path = cache_list.splice( 0, 1 )[0];
+				delete cache_map[remove_path];
+				console.log( '[fileCacher] Remove >> ' + remove_path );
+			}
+			
+			cache_list.push( path );
+			cache_map[path] = data;
+			
+			callback( err, data );
+		});
+	};
+	
+	inst.init = init;
+	inst.clear = clear;
+	inst.getFile = getFile;
+	
+	return inst;
+} ());
+
+const MasterEvent = {
+	  CACHE_GET: 'CACHE_GET'
+	, CACHE_SET: 'CACHE_SET'
+	, CACHE_UPDATE: 'CACHE_UPDATE'
+	, CLEAR_CHILD_CACHE: 'CLEAR_CHILD_CACHE'
+	, CLEAR_MASTER_CACHE: 'CLEAR_MASTER_CACHE'
+	, MESSAGE: 'MESSAGE'
+};
+
+var app = ( function () {
+	var inst = {};
+	
+	let server = express();
+	
+	var cacheData = {};
+	
+	var actAPI = function ( query, res ) {
+		if ( !validator.check( query, [
+			{ name: 'act', type: 'string' }
+		] ) ) {
+			res.writeHead(404);
+			res.end( 'Not found' );
+			console.log( 'actAPI fail' );
+			return;
+		}
+		
+		var action = query.act;
+		switch ( action ) {
+		case 'assets_group_name_list':
+			var map = {};
+			if ( cacheData.hasOwnProperty( 'assets_group_name' ) ) {
+				map = cacheData.assets_group_name;
+			}
+			res.setHeader('Content-Type', 'application/json');
+			res.setHeader('Cache-Control', 'no-cache, no-store');
+			res.end( JSON.stringify( map ) );
+			return;
+		case 'assets_group_name_update':
+			if ( !validator.check( query, [
+				{ name: 'name', type: 'string' },
+				{ name: 'value', type: 'string' }
+			] ) ) {
+				res.writeHead(404);
+				res.end( 'act fail: ' + action );
+				console.log( 'actAPI fail: ' + action );
+				return;
+			}
+			
+			var name = query.name;
+			var value = query.value;
+			
+			var map = {};
+			if ( cacheData.hasOwnProperty( 'assets_group_name' ) ) {
+				map = cacheData.assets_group_name;
+			}
+			
+			var isUpdateCache = false;
+			
+			if ( !map.hasOwnProperty( name ) ) {
+				isUpdateCache = true;
+				map[name] = { value: name, lock: 5 };
+			}
+			
+			if ( map[name].lock > 0 && map[name].value !== value ) {
+				isUpdateCache = true;
+				map[name].value = value;
+				map[name].lock--;
+			}
+			
+			res.writeHead(200);
+			
+			if ( isUpdateCache ) {
+				cacheData.assets_group_name = map;
+				eventDispatcher.dispatch( MasterEvent.CACHE_UPDATE, { name: 'assets_group_name', method: 'property', key: name, value: map[name] } );
+				eventDispatcher.dispatch( MasterEvent.CACHE_UPDATE, { name: 'assets_group_update', method: 'push', value: { name: name, value: map[name].value } } );
+				res.end( 'update success: assets_group_name [' + name +  '] ( lock: ' + map[name].lock + ' ) as ' + map[name].value );
 			} else {
-				var data = JSON.parse(rslt);
-				if ( data.hasOwnProperty( 'log' ) ) {
-					data = data.log;
-					if ( data.hasOwnProperty( 'entries' ) ) {
-						data = data.entries;
-						if ( data instanceof Array ) {
-							for(var i = 0; i < data.length; i++) {
-								var item = data[i];
-								if ( !item.hasOwnProperty( 'request' ) )
-								{
-									continue;
-								}
-								item = item.request;
-								if ( !item.hasOwnProperty( 'url' ) )
-								{
-									continue;
-								}
-								var url = item.url;
-								var type = url.substr( url.length - 36, 3 );
-								if ( type === 'swf' ) {
-									var pos = url.lastIndexOf( '/' );
-									var shortName = url.substr( pos + 1 );
-									shortName = shortName.substr( 0, shortName.length - 33 );
-									if ( !swfMap[shortName] ) {
-										swfMap[shortName] = true;
-										swfList.push( { url: url, name: shortName } );
-									}
-								}
-							}
-						}
+				res.end( 'update rejected: assets_group_name [' + name +  '] ( lock: ' + map[name].lock + ' ) as ' + map[name].value );
+			}
+			
+			return;
+		case 'cache':
+			res.setHeader('Content-Type', 'application/json');
+			res.setHeader('Cache-Control', 'no-cache, no-store');
+			res.end(JSON.stringify(cacheData));
+			return;
+		}
+		
+		res.writeHead(404);
+		res.end( 'Not found: ' + action );
+	};
+	
+	var sendFile = function ( url, res, forceReload ) {
+		var urlFinal = './static' + url;
+		fileCacher.getFile( urlFinal, forceReload, function(err, data) {
+			if (err) {
+				res.writeHead(404);
+				res.end( 'Not found: ' + url );
+				console.log( 'sendFile fail >> ' + urlFinal );
+			} else {
+				let ext = path.extname(url).slice(1);
+				switch ( ext ) {
+				case 'swf':
+					res.setHeader('Content-Type', 'application/x-shockwave-flash');
+					break;
+				default:
+					if (contentTypes[ext]) {
+						res.setHeader('Content-Type', contentTypes[ext]);
 					}
 				}
-				fetchSwf();
+
+				if (ext === 'html') {
+					res.setHeader('Cache-Control', 'no-cache, no-store');
+				} else {
+					res.setHeader('Cache-Control', 'max-age=604800, public');
+				}
+				res.end(data);
+				console.log( 'sendFile ok >> ' + urlFinal );
 			}
 		});
 	};
 	
-	fs.readdir('./', function(err, items) {
-		for (var i = 0; i < items.length; i++) {
-			var type = items[i].substr( items[i].length - 3, 3 );
-			if ( type === 'har' ) {
-				console.log( 'har found >> ' + items[i] );
-				harList.push( items[i] );
-			}
-		}
-		fetchSwf();
-	});
-};
+	server.get('/*', function (req, res) {
+		let url = req.path;
+		
+		console.log( 'Get: ' + url + ': ' + JSON.stringify( req.query ) );
+		
+		// IMPORTANT: Your application HAS to respond to GET /health with status 200
+		//			for OpenShift health monitoring
 
-var task_test = function ( callback ) {
+		switch ( url ) {
+		case '/api':
+			actAPI( req.query, res );
+			break;
+		case '/health':
+			res.writeHead(200);
+			res.end();
+			break;
+		case '/info/gen':
+		case '/info/poll':
+			res.setHeader('Content-Type', 'application/json');
+			res.setHeader('Cache-Control', 'no-cache, no-store');
+			res.end(JSON.stringify(sysInfo[url.slice(6)]()));
+			break;
+		case '/clear':
+			eventDispatcher.dispatch( MasterEvent.CLEAR_CHILD_CACHE );
+			res.writeHead(200);
+			res.end();
+			break;
+		case '/cron':
+			res.writeHead(200);
+			res.end();
+			break;
+		case '/':
+		case '/avatar':
+			eventDispatcher.dispatch( MasterEvent.CACHE_UPDATE, { name: 'visit', value: 1, method: 'add' } );
+			sendFile( '/avatar.html', res, false );
+			break;
+		default:
+			sendFile( url, res, false );
+		}
+	});
 	
-	var assetsList = [];
-	var groupList = [];
-	
-	var analysisFile = function ( name ) {
-		var rslt = {
-			  pass: false
-			, group: ''
-			, name: name
-			, order: 0
-		};
-		var dotPos = name.lastIndexOf( '.swf' );
-		var pPos = name.lastIndexOf( '_p' );
-		var checkStr = name.substr( pPos + 2, dotPos - pPos - 2 );
-		if ( '' + parseInt( checkStr ) !== checkStr ) {
-			return rslt;
+	var onCacheGet = function ( data ) {
+		if ( !validator.check( data, [
+			{ name: 'name', type: 'string' }
+		] ) ) {
+			return;
 		}
 		
-		rslt.pass =  true;
-		rslt.group = name.substr( 0, pPos );
-		rslt.name = name.substr( 0, dotPos );
-		rslt.order = parseInt( checkStr );
-		return rslt;
+		process.send( { action: 'get', name: data.name } );
 	};
 	
-	var onReadDirReady = function ( err, rslt ) {
-		buildAssetsList( rslt );
-		buildGroupList();
-		var str = buildEmbedCode();
-		callback( str );
+	var onCacheSet = function ( data ) {
+		if ( !validator.check( data, [
+			{ name: 'name', type: 'string' },
+			{ name: 'value' }
+		] ) ) {
+			return;
+		}
+		
+		process.send( { action: 'set', name: data.name, value: data.value } );
 	};
 	
-	var buildAssetsList = function ( list ) {
-		for (var i = 0; i < list.length; i++) {
-			var analyRslt = analysisFile( list[i] );
-			if ( !analyRslt.pass ) {
-				console.log( 'fail : ' + analyRslt.name );
-				continue;
-			}
+	var onCacheUpdate = function ( data ) {
+		if ( !validator.check( data, [
+			{ name: 'name', type: 'string' },
+			{ name: 'method', type: 'string' },
+			{ name: 'value' }
+		] ) ) {
+			console.log( 'onCacheUpdate fail' );
+			return;
+		}
+		
+		data.action = 'update';
+		
+		process.send( data );
+	};
+	
+	var onClearChildCache = function ( data ) {
+		process.send( { action: 'clear_child', } );
+	};
+	
+	var onClearMasterCache = function ( data ) {
+		process.send( { action: 'clear' } );
+	};
+	
+	var onMessage = function ( value ) {
+		process.send( { action: 'message', value: '' + value } );
+	};
+
+	var start = function () {
+		
+		eventDispatcher.add( MasterEvent.CACHE_GET, onCacheGet );
+		eventDispatcher.add( MasterEvent.CACHE_SET, onCacheSet );
+		eventDispatcher.add( MasterEvent.CACHE_UPDATE, onCacheUpdate );
+		eventDispatcher.add( MasterEvent.CLEAR_CHILD_CACHE, onClearChildCache );
+		eventDispatcher.add( MasterEvent.CLEAR_MASTER_CACHE, onClearMasterCache );
+		eventDispatcher.add( MasterEvent.MESSAGE, onMessage );
+		
+		server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
+			process.on( 'message', function( data ) {
+				var action = data.action;
+				switch ( action ) {
+				case 'clear_cache':
+					fileCacher.clear();
+					console.log( 'Clear cache of ' + process.pid );
+					break;
+				case 'update_cache':
+					cacheData = data.data;
+					console.log( 'Update cache of ' + process.pid + ': length >> ' + JSON.stringify( cacheData ).length );
+					break;
+				}
+			});
 			
-			assetsList.push( analyRslt );
-		}
-		console.log( 'assets found : ' + assetsList.length );
-	};
-	
-	var buildGroupList = function () {
-		var groupMap = {};
-		for (var i = 0; i < assetsList.length; i++) {
-			var item = assetsList[i];
-			var group = null;
-			if ( !groupMap.hasOwnProperty( item.group ) ) {
-				group = {};
-				group.name = item.group;
-				group.parts = [];
-				groupList.push( group );
-				groupMap[item.group] = group;
-			} else {
-				group = groupMap[item.group];
-			}
-			group.parts.push( item.order );
-		}
-		console.log( 'groups found : ' + groupList.length );
-	};
-	
-	var buildEmbedCode = function () {
-		var str = "";
-		for (var i = 0; i < assetsList.length; i++) {
-			var item = assetsList[i];
-			str += '[Embed(source = "../../../../assets/' + item.name + '.swf")]<br/>';
-			str += 'private var ' + item.name + ':Class;<br/>';
-		}
-		for (var i = 0; i < assetsList.length; i++) {
-			var item = assetsList[i];
-			str += 'this._map["' + item.name + '"] = new ' + item.name + '();<br/>';
-		}
-		return str;
-	};
-	
-	fs.readdir( './assets/', onReadDirReady );
-};
+			process.send( { action: 'init', pid: process.pid } );
 
-app.get('/', function (req, res) {
-	console.log( 'Start:' );
-	res.writeHeader( 200, { "Content-Type": "text/html" } );
-	var callback = function ( rslt ) {
-		res.write( rslt );
-		res.end();
-		console.log( 'End:' );
-	};
-	var actFunc = function ( callback ) {
-		callback( 'No task called' );
-	};
-	switch ( currTask ) {
-	case TASK.FETCH_SWF:
-		actFunc = task_fetch_swf;
-		break;
-	case TASK.TEST:
-		actFunc = task_test;
-		break;
-	default:
-		;
-	}
-	actFunc( callback );
-});
-
-app.get('/avatar', function (req, res) {
-	console.log( 'Start:' );
-	res.writeHeader( 200, { "Content-Type": "text/html" } );
-	var callback = function ( rslt ) {
-		res.write( rslt );
-		res.end();
-		console.log( 'End:' );
+			eventDispatcher.dispatch( MasterEvent.MESSAGE, 'Application worker started...' );
+		});
 	};
 	
-	fs.readFile('./avatar.html', function (err, data) {
-		if (err) {
-			throw err; 
-		}
-		
-		callback( data );
-	});
-});
-
-app.get('/assets.swf', function (req, res) {
-	console.log( 'Start:' );
-	res.writeHeader( 200, { "Content-Type": "application/x-shockwave-flash" } );
-	var callback = function ( rslt ) {
-		res.write( rslt );
-		res.end();
-		console.log( 'End:' );
-	};
+	fileCacher.init( 10 );
 	
-	fs.readFile('./assets.swf', function (err, data) {
-		if (err) {
-			throw err; 
-		}
-		
-		callback( data );
-	});
-});
+	inst.start = start;
+	
+	return inst;
+} ());
 
-app.listen(5123, function () {
-	console.log('Example app listening on port 5123!');
-});
+app.start();
